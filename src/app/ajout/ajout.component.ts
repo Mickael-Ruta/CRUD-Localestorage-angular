335a6ff9d75d1e3318bc32870b26c692465e002a
Ruta Mickaël <102019371+Mickael-Ruta@users.noreply.github.com>
2024-02-02 07:39:16 +0100

    recherche

--- src/app/ajout/ajout.component.ts
+++ src/app/ajout/ajout.component.ts
@@ -1,137 +1,151 @@
-import {  AfterViewInit, Component, Inject, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Inject, OnInit } from '@angular/core';
 import { Iliste, ListeComponent } from '../liste/liste.component';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 
-
-declare const M:any
+declare const M: any;
 @Component({
   selector: 'app-ajout',
   standalone: true,
-  imports: [ReactiveFormsModule,],
+  imports: [ReactiveFormsModule],
   templateUrl: './ajout.component.html',
   styleUrl: './ajout.component.scss',
-  providers :[]
+  providers: [],
 })
-export class AjoutComponent implements OnInit , AfterViewInit {
-
-  constructor (private fb : FormBuilder,private router :Router,private routeActive : ActivatedRoute){}
+export class AjoutComponent implements OnInit, AfterViewInit {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private routeActive: ActivatedRoute
+  ) {}
 
   // les interfaces
   ngOnInit(): void {
-    this.formControlName()
-    this.get(this.list)
-    this.etuFormvalueInit()
+    this.formControlName();
+    this.get(this.dataName);
+    this.etuFormvalueInit();
   }
 
   ngAfterViewInit(): void {
-    if (typeof M !== 'undefined'){
-      M.AutoInit()
+    if (typeof M !== 'undefined') {
+      M.AutoInit();
     }
   }
   // les proprietes
-  public form! : FormGroup
-  public data : Iliste[]=[]
-  public list : string = 'data'
-  public formTitle : string = 'Ajouter'
-  public btnTitle : string = 'Ajouter'
+  public form!: FormGroup;
+  public data: Iliste[] = [];
+  public dataName: string = 'data';
+  public formTitle: string = 'Ajouter';
+  public btnTitle: string = 'Ajouter';
+  public modif: boolean = false;
 
-  public modif : boolean =false
-
-// les methodes
-  public get(parm : string){
-    if(typeof localStorage != 'undefined'){
-      const getData = localStorage.getItem(parm)
-      this.data=JSON.parse(getData!)
+  // les methodes
+  public get(parm: string) {
+    if (typeof localStorage != 'undefined') {
+      const getData = localStorage.getItem(parm);
+      this.data = JSON.parse(getData!);
     }
   }
-  public formControlName(){
-    this.form=this.fb.group({
-      matricule : ['',Validators.required],
-      nom : ['',Validators.required],
-      prenoms : ['',Validators.required],
-      dateNaiss : ['',Validators.required],
-      mention : ['DA2I',Validators.required],
-      niveau : ['L1',Validators.required],
-    })
+  public formControlName() {
+    this.form = this.fb.group({
+      matricule: ['', Validators.required],
+      nom: ['', Validators.required],
+      prenoms: [''],
+      dateNaiss: ['', Validators.required],
+      mention: ['DA2I', Validators.required],
+      niveau: ['L1', Validators.required],
+    });
   }
 
-  public matriculeIsExist(num :string){
-    const tab = this.data.map(tab => tab.matricule)
-    const res = tab.indexOf(num)
-
-    return res
+  public matriculeIsExist(num: string, tableau: Iliste[]) {
+    const tab = tableau.map((tab) => tab.matricule);
+    const res = tab.indexOf(num);
+    return res;
   }
 
-  public addData(){
-    if(typeof localStorage !== 'undefined'){
-      if(this.form.valid){
-        const addEtu=this.form.value
-        const exist =this.matriculeIsExist(addEtu.matricule)
-        if(exist===-1){
-          this.data.push(addEtu)
-          const newData = JSON.stringify(this.data)
-          localStorage.setItem(this.list,newData)
-          alert('Ajout avec succes')
-          this.form.reset()
-          this.router.navigate(['/liste'])
-        }else{
-          alert('Ce numero matricule existe deja')
-          this.form.setValue({...this.form.value,
-            matricule : ''
-          })
+  public addData() {
+    if (typeof localStorage !== 'undefined') {
+      if (this.form.valid) {
+        const addEtu = this.form.value;
+        if (!!this.data) {
+          const exist = this.matriculeIsExist(addEtu.matricule, this.data);
+          if (exist === -1) {
+            this.data.push(addEtu);
+            const newData = JSON.stringify(this.data);
+            localStorage.setItem(this.dataName, newData);
+            alert('Ajout avec succes');
+            this.form.reset();
+            this.router.navigate(['/liste']);
+          } else {
+            alert('Ce numero matricule existe deja');
+            this.form.setValue({ ...this.form.value, matricule: '' });
+          }
+        } else {
+          this.data = [this.form.value];
+          const newData = JSON.stringify(this.data);
+          localStorage.setItem(this.dataName, newData);
+          alert('Ajout avec succes');
+          this.form.reset();
+          this.router.navigate(['/liste']);
         }
-      }else{
-        alert('Veuilez completer tout les champs')
+      } else {
+        alert('Veuilez completer tout les champs');
       }
     }
   }
 
-  public update(){
-    const matricule = this.routeActive.snapshot.paramMap.get('matricule')
-    if(matricule){
-      if(typeof localStorage !== 'undefined'){
-        if(this.form.valid){
-          const updEtu = this.form.value
-          const indexEtuUpdate = this.data.findIndex((etu)=>etu.matricule===matricule)
-          this.data.splice(indexEtuUpdate,1,updEtu)
+  public update() {
+    const matricule = this.routeActive.snapshot.paramMap.get('matricule');
+    if (matricule) {
+      if (typeof localStorage !== 'undefined') {
+        if (this.form.valid) {
+          const updEtu = this.form.value;
+          const indexEtuUpdate = this.data.findIndex(
+            (etu) => etu.matricule === matricule
+          );
+          this.data.splice(indexEtuUpdate, 1, updEtu);
 
-          localStorage.setItem(this.list,JSON.stringify(this.data))
-          alert('modification reussi')
+          localStorage.setItem(this.dataName, JSON.stringify(this.data));
+          alert('modification reussi');
 
-          this.router.navigate(['/liste'])
-        }
-        else{
-          alert('Remplis le fomulaire s\'il vous plait')
+          this.router.navigate(['/liste']);
+        } else {
+          alert("Remplis le fomulaire s'il vous plait");
         }
       }
     }
   }
 
+  public etuFormvalueInit() {
+    const matricule = this.routeActive.snapshot.paramMap.get('matricule');
+    if (matricule) {
+      this.formTitle = 'Modifier';
+      this.btnTitle = 'Modifier';
+      this.modif = true;
 
-  public etuFormvalueInit(){
-    const matricule = this.routeActive.snapshot.paramMap.get('matricule')
-    if(matricule){
-      this.formTitle='Modifier'
-      this.btnTitle='Modifier'
-      this.modif=true
-
-      const indexEtuUpdate = this.data.findIndex((etu)=>etu.matricule===matricule)
-      const dataETu = this.data[indexEtuUpdate]
+      const indexEtuUpdate = this.data.findIndex(
+        (etu) => etu.matricule === matricule
+      );
+      const dataETu = this.data[indexEtuUpdate];
 
       this.form.patchValue({
-        matricule : dataETu.matricule,
-        nom : dataETu.nom,
-        prenoms : dataETu.prenoms,
-        dateNaiss : dataETu.dateNaiss,
-        mention : dataETu.mention,
-        niveau : dataETu.niveau
-      })
-    }else{
-      this.formTitle='Ajouter'
-      this.btnTitle='Ajouter'
+        matricule: dataETu.matricule,
+        nom: dataETu.nom,
+        prenoms: dataETu.prenoms,
+        dateNaiss: dataETu.dateNaiss,
+        mention: dataETu.mention,
+        niveau: dataETu.niveau,
+      });
+    } else {
+      this.formTitle = 'Ajouter';
+      this.btnTitle = 'Ajouter';
     }
-
   }
-}
 
+  // recherche
+}
--- src/app/liste/liste.component.ts
+++ src/app/liste/liste.component.ts
@@ -1,72 +1,96 @@
-import { AfterViewInit, Component,OnInit} from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { AjoutComponent } from '../ajout/ajout.component';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { DATE_PIPE_DEFAULT_OPTIONS, DatePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { range } from 'rxjs';
-
-
+import { DatePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 
 export interface Iliste {
-  matricule : string  ,
-  nom :string  ,
-  prenoms : string  ,
-  dateNaiss : Date ,
-  mention : string  ,
-  niveau : string
+  matricule: string;
+  nom: string;
+  prenoms: string;
+  dateNaiss: Date;
+  mention: string;
+  niveau: string;
 }
 
-declare const M : any;
+declare const M: any;
 @Component({
   selector: 'app-liste',
   standalone: true,
-  imports: [RouterModule,FormsModule,UpperCasePipe,TitleCasePipe,DatePipe],
+  imports: [RouterModule, FormsModule, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './liste.component.html',
   styleUrl: './liste.component.scss',
-  providers : [AjoutComponent,]
+  providers: [AjoutComponent],
 })
-
-export class ListeComponent implements OnInit,AfterViewInit{
-  constructor (private ajout : AjoutComponent){}
+export class ListeComponent implements OnInit, AfterViewInit {
+  constructor(private ajout: AjoutComponent) {}
 
   ngOnInit(): void {
-    this.data=this.ajout.data
+    this.data = this.ajout.data;
+    this.filteredEtu = this.data;
+    console.log(this.filteredEtu, this.EtudiantFilter);
   }
 
   ngAfterViewInit(): void {
-    if (typeof M !=='undefined') {
+    if (typeof M !== 'undefined') {
       M.AutoInit();
     }
   }
 
-// propriete
-  public data:Iliste[]=[]
-  public dataName ='data'
-  public filtre!: string
-  public selecteditems!:Iliste
+  // propriete
+  public data: Iliste[] = [];
+  public dataName = 'data';
+  public filtre!: string;
+  public selecteditems!: Iliste;
 
-// methode
-  public get = this.ajout.get(this.dataName)
+  // methode
 
+  public get = this.ajout.get(this.dataName);
   trackBy(index: number, item: any): number {
     return item.matricule;
   }
-
-  public open(list : Iliste){
-    this.selecteditems = list
+  public open(list: Iliste) {
+    this.selecteditems = list;
   }
+  public delete(item: Iliste): void {
+    this.selecteditems = item;
+    const nomItems = item.nom;
+    const indexItem = this.data.findIndex((res) => res.nom === nomItems);
+    this.data.splice(indexItem, 1);
+    const newData = this.data;
 
-
-  public delete(item:Iliste):void{
-    this.selecteditems = item
-    const nomItems = item.nom
-    const indexItem = this.data.findIndex((res)=>res.nom===nomItems)
-    this.data.splice(indexItem,1)
-    const newData = this.data
-
-    if(typeof localStorage !== 'undefined'){
-      localStorage.setItem(this.dataName,JSON.stringify(newData))
+    if (typeof localStorage !== 'undefined') {
+      localStorage.setItem(this.dataName, JSON.stringify(newData));
     }
   }
 
+  // filtre
+  private _EtuFilter: string = '';
+  public filteredEtu: Iliste[] = [];
+
+  public get EtudiantFilter(): string {
+    return this._EtuFilter;
+  }
+
+  public set EtudiantFilter(filter: string) {
+    this._EtuFilter = filter;
+    this.filteredEtu = this.EtudiantFilter
+      ? this.filterEtu(this.EtudiantFilter)
+      : this.data;
+  }
+
+  private filterEtu(critere: string): Iliste[] {
+    critere = critere.toLowerCase();
+
+    const res = this.data.filter(
+      (reponse: Iliste) =>
+        reponse.nom.toLowerCase().indexOf(critere) != -1 ||
+        reponse.prenoms.toLowerCase().indexOf(critere) != -1 ||
+        reponse.matricule.toLowerCase().indexOf(critere) != -1 ||
+        reponse.mention.toLowerCase().indexOf(critere) != -1 ||
+        reponse.niveau.toLowerCase().indexOf(critere) != -1
+    );
+
+    return res;
+  }
 }
